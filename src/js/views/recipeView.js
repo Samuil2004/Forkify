915db3773a8c6f692da49295c700ce81c47197a1
Samuil2004 <s.kozarov23@acsbg.org>
2024-02-06 12:15:51 +0100

    Fracty is added instead of Fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,6 @@ import View from './View.js';
 import icons from 'url:../../img/icons.svg';
 import fracty from 'fracty';
 
-
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
   _errorMessage = 'We could not find that recipe. Please try another one!';
